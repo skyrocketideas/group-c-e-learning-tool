1b3c6903ebf0b1e26a12001e4fb35fad662d9a64
Lasse Stausgaard <L.stausgaard@gmail.com>
2021-05-20 13:06:35 +0200

    something

--- models/article.js
+++ models/article.js
@@ -2,16 +2,23 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const articleSchema = new Schema({
-	title: {
+	name: {
 		type: String,
 	},
-	body: {
+	description: {
 		type: String,
 	},
-	color: {
+	text: {
 		type: String,
 	},
+	link: {
+		type: String,
+	}
 });
+
+
+
+
 
 const Article = mongoose.model("Article", articleSchema);
 module.exports = Article;
